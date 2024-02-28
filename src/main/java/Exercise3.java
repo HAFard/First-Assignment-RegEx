b2f84a1eeaa44be3594741609a2d992de56de230
HAFard <f.hananeh@gmail.com>
2024-02-28 17:02:45 +0330

    add bonus problem

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -53,7 +53,7 @@ public class Exercise3 {
         }
 
         return wordsWithRepeatLetters;
-        // TODO
+
     }
 
     /*
@@ -64,8 +64,16 @@ public class Exercise3 {
 
     public static List<String> findReapetdWords(String input) {
         List<String> repeatedWords = new ArrayList<>();
+        String regex = "\\b([A-Za-z]+)+\\1\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            repeatedWords.add( matcher.group());
+        }
+
         return repeatedWords;
-        // TODO
     }
 
     public static void main(String[] args) {
